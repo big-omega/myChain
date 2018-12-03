506b1ffe8bf538c8763bc9a5a9106189ed0a4638
zfgao <zfgao1994@sjtu.edu.cn>
2018-12-03 20:37:16 +0800

    add PoW module

--- block.go
+++ block.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
@@ -12,22 +9,17 @@ type Block struct {
 	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
+	Nonce         int
 	Hash          []byte
-}
-
-// SetHash fills in the hash field of a block
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
 }
 
 // NewBlock is a constructor for a block
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, 0, []byte{}}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
 
 	return block
 }
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 )
+
+func init() {
+	log.SetPrefix("DEBUG: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
+}
 
 // main is the entry point for the program
 func main() {
-	fmt.Println("Welcome to my first blockchain\n")
+	fmt.Println("Welcome to my first blockchain")
 
 	bc := NewBlockchain()
 
@@ -14,9 +21,10 @@ func main() {
 	bc.AddBlock("Send 2 more BTC to bigOmega")
 
 	for _, block := range bc.blocks {
-		fmt.Printf("PrevHash: %x\n", block.PrevBlockHash)
+		fmt.Printf("\nPrevHash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println("")
+		fmt.Printf("PoW: %s", strconv.FormatBool(NewProofOfWork(block).Validate()))
+		fmt.Println()
 	}
 }
